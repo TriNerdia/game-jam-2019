7953ddc4f6039859d5147e4af7dab0ae9d94871f
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 13:21:21 -0500

    added enemy prafab, pickup destruction, and enemy spawning (#4)

--- Game/Assets/SpawnEnemy.cs
+++ Game/Assets/SpawnEnemy.cs
@@ -7,8 +7,9 @@ public class SpawnEnemy : MonoBehaviour
 {
     public GameObject Enemy;
     public int MaxEnemyCount = 5;
-        // Start is called before the first frame update
-        void Start()
+
+    // Start is called before the first frame update
+    private void Update()
     {
         for (int i = 0; i < MaxEnemyCount; i++)
         {
@@ -17,8 +18,4 @@ public class SpawnEnemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
