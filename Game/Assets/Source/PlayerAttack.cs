1daa250eabc7581275d05b02100dca57214d6e13
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 18:08:56 -0500

    Adding initial player attack animation

---
+++ Game/Assets/Source/PlayerAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public SwordAttack sword;
+
+    public KeyCode attackKey = KeyCode.Space;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(attackKey))
+        {
+            sword.PlayAttackAnimation();
+        }
+    }
+}
