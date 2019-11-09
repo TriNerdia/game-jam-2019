eab30af1e2a0625832f379acc2a371ec51edac6a
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 16:44:41 -0500

    Adding attack animation to red sphere enemy (#8)

---
+++ Game/Assets/Enemy/RedSphere/SwordAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Animation swordAnimation;
+
+    private void Start()
+    {
+        swordAnimation = GetComponent<Animation>();
+    }
+
+    public void PlayAttackAnimation()
+    {
+        swordAnimation.Play("Attack");
+    }
+}
