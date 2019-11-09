673853379e405b2ee15e1ba08c0dbdd07734bfcf
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 16:30:38 -0500

    Adding attack animation to red sphere enemy

---
+++ Game/Assets/Enemy/RedSphere/SphereEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereEnemy : MonoBehaviour
+{
+    public SwordAttack sword;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
