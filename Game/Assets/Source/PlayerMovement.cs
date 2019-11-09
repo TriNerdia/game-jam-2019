48c05883ef08b2e2f82257fd892581bb40f90c81
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 11:28:52 -0500

    Adding initial player model

---
+++ Game/Assets/Source/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+
+    public Vector3 Direction;
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+
+    public void Update()
+    {
+        Direction = Vector3.zero;
+
+        if (Input.GetKey(moveUp))
+        {
+            Direction.y += 1;
+        }
+
+        if (Input.GetKey(moveDown))
+        {
+            Direction.y -= 1;
+        }
+
+        if (Input.GetKey(moveRight))
+        {
+            Direction.x += 1;
+        }
+
+        if (Input.GetKey(moveLeft))
+        {
+            Direction.x -= 1;
+        }
+
+        if (Direction.magnitude > 1)
+        {
+            Direction.Normalize();
+        }
+
+        transform.Translate(Direction * MoveSpeed * Time.deltaTime);
+    }
+}
