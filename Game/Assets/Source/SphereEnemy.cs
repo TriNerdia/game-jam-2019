c70b707b35e2e052da6c112193af66b7f9e9ac0f
Max Mitschke <maxmitschke@fastmail.com>
2019-11-10 13:04:37 -0500

    Enemy / Player do damage to each other (#22)

--- Game/Assets/Source/SphereEnemy.cs
+++ Game/Assets/Source/SphereEnemy.cs
@@ -6,14 +6,39 @@ public class SphereEnemy : MonoBehaviour
 {
     public SwordAttack sword;
 
+    public string TargetTag = "Player";
+    public bool IsAttacking { get; internal set; }
+
     public KeyCode attackKey = KeyCode.L;
 
-    // Update is called once per frame
+    void Start()
+    {
+        IsAttacking = false;
+    }
+
     void Update()
     {
-        if (Input.GetKey(attackKey))
+        if (Input.GetKey(attackKey) || IsAttacking)
         {
             sword.PlayAttackAnimation();
         }
+    }
+
+    // Look at the second sphere collider that has the IsTrigger button set
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag(TargetTag))
+        {
+            IsAttacking = true;
+            transform.LookAt(collider.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag(TargetTag))
+        {
+            IsAttacking = false;
+        }   
     }
 }
