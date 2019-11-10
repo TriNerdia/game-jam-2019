27148db1e03850614fe20f66ac7123f3bf2ceea8
Max Mitschke <maxmitschke@fastmail.com>
2019-11-10 09:23:30 -0500

    scene transitions working (#20)

---
+++ Game/Assets/Source/GameoverMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameoverMenuScript : MonoBehaviour
+{
+    public Button restartBtn;
+
+    void Start()
+    {
+        Button rbtn = restartBtn.GetComponent<Button>();
+        rbtn.onClick.AddListener(Scenechange);
+    }
+
+    void Scenechange()
+    {
+        SceneManager.LoadScene("TestField", LoadSceneMode.Single);
+    }
+}
