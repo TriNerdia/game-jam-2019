ce918643ebc7e864580f8d387d0aae15354208db
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 23:07:14 -0500

    Enemy follows player with navmesh (#19)

--- Game/Assets/chasePlayer.cs
+++ Game/Assets/chasePlayer.cs
@@ -3,13 +3,18 @@ using UnityEngine.AI;
 
 public class chasePlayer : MonoBehaviour
 {
-
-    public Transform goal;
+    NavMeshAgent agent;
+    Transform goal;
 
     void Start()
     {
         goal = GameObject.FindGameObjectWithTag("Player").transform;
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    void Update()
+    {
         agent.destination = goal.position;
     }
 }
