2ab801c65ea7334f1b3869d67c5804e92f12d05f
Max Mitschke <maxmitschke@fastmail.com>
2019-11-09 20:38:51 -0500

    Fixed player camera rotation
    The previous implementation would cause people to barf.

---
+++ Game/Assets/Source/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
