e77b72bca589171ffd61c826b9fe1a4694e55788
Max Mitschke <maxmitschke@fastmail.com>
2019-11-10 14:12:00 -0500

    Enemies stop and widened detection sphere (#25)
    * Enemies stop and widened detection sphere
    
    * Reduced health back to 80

--- Game/Assets/chasePlayer.cs
+++ Game/Assets/chasePlayer.cs
@@ -5,16 +5,27 @@ public class chasePlayer : MonoBehaviour
 {
     NavMeshAgent agent;
     Transform goal;
+    public SphereEnemy obj;
+    public float speed;
 
     void Start()
     {
         goal = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+        obj = GetComponent<SphereEnemy>();
     }
 
     void Update()
     {
+        if (obj.IsAttacking)
+        {
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = 3;
+        }
         agent.destination = goal.position;
     }
 }
