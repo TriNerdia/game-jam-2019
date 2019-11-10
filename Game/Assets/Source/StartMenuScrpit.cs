2ec0545b7f95f512e8836b6466e84fd303e32fc9
kirbyftw <kirbyftw64@gmail.com>
2019-11-10 14:05:29 -0500

    Menu buttons updated, restart added, quit function added

--- Game/Assets/Source/StartMenuScrpit.cs
+++ Game/Assets/Source/StartMenuScrpit.cs
@@ -7,15 +7,23 @@ using UnityEngine.SceneManagement;
 public class StartMenuScrpit : MonoBehaviour
 {
     public Button startBtn;
+    public Button quitBtn;
 
     void Start()
     {
         Button rbtn = startBtn.GetComponent<Button>();
+        Button qbtn = quitBtn.GetComponent<Button>();
         rbtn.onClick.AddListener(StartGame);
+        qbtn.onClick.AddListener(QuitGame);
     }
 
     void StartGame()
     {
         SceneManager.LoadScene("Maze 1", LoadSceneMode.Single);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
     }
 }
