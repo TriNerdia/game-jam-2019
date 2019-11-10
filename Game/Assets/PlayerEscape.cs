113dd854ebb1a490849b3fc6f63f9be6cb302cb5
Max Mitschke <maxmitschke@fastmail.com>
2019-11-10 15:19:51 -0500

    Player now wins the game by destroying the portals (#28)

---
+++ Game/Assets/PlayerEscape.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerEscape : MonoBehaviour
+{
+
+    [SerializeField] TMP_Text displayText;
+
+    [SerializeField] PlayerAttack pAttack;
+
+    bool triggering = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("disableStartText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator disableStartText()
+    {
+        yield return new WaitForSeconds(5);
+        if(!triggering)
+            displayText.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            triggering = true;
+            displayText.gameObject.SetActive(true);
+
+            if (GameHandler.PortalsDestroyed)
+                displayText.text = "All Portals destroyed.  Attack to escape!";
+            else
+                displayText.text = GameHandler.PortalsLeft + " portals remain.  Destroy them and return here!";
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            triggering = false;
+            displayText.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (GameHandler.PortalsDestroyed && Input.GetKeyDown(pAttack.attackKey)){
+                SceneManager.LoadScene("Game Winner Menu", LoadSceneMode.Single);
+            }
+        }
+    }
+
+}
