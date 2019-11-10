6357827671cd8c86f8d4cd72a39b49bd0bcb18d9
Max Mitschke <maxmitschke@fastmail.com>
2019-11-10 14:12:48 -0500

    Menu buttons updated, restart added, quit function added (#26)

--- Game/Assets/Source/GameoverMenuScript.cs
+++ Game/Assets/Source/GameoverMenuScript.cs
@@ -7,15 +7,31 @@ using UnityEngine.SceneManagement;
 public class GameoverMenuScript : MonoBehaviour
 {
     public Button restartBtn;
+    public Button menuBtn;
+    public Button quitBtn;
 
     void Start()
     {
         Button rbtn = restartBtn.GetComponent<Button>();
-        rbtn.onClick.AddListener(Scenechange);
+        Button mbtn = menuBtn.GetComponent<Button>();
+        Button qbtn = quitBtn.GetComponent<Button>();
+        rbtn.onClick.AddListener(ScenechangeGame);
+        mbtn.onClick.AddListener(ScenechangeMenu);
+        qbtn.onClick.AddListener(QuitGame);
     }
 
-    void Scenechange()
+    void ScenechangeMenu()
     {
         SceneManager.LoadScene("Opening Menu", LoadSceneMode.Single);
+    }
+
+    void ScenechangeGame()
+    {
+        SceneManager.LoadScene("Maze 1", LoadSceneMode.Single);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
     }
 }
