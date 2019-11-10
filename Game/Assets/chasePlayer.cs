f617b3905dfaa2dcf25a087894550a07923c8342
Arlodu <aronlondond@gmail.com>
2019-11-09 21:06:48 -0500

    added nave mesh

---
+++ Game/Assets/chasePlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class chasePlayer : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
