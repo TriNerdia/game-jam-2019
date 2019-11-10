45e863a2509a8c8a037b9b67172a73c7f2f7091e
kirbyftw <kirbyftw64@gmail.com>
2019-11-10 16:13:42 -0500

    Pause created

--- Game/Assets/GameHandler.cs
+++ Game/Assets/GameHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameHandler : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class GameHandler : MonoBehaviour
     public static bool PortalsDestroyed { get => GameObject.FindGameObjectsWithTag("Portal").Length == 0; }
 
     public static int PortalsLeft { get => GameObject.FindGameObjectsWithTag("Portal").Length; }
+
+    [SerializeField] TMP_Text displayText;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,33 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                pauseGame();
+            }
+            else
+            {
+                coutinueGame();
+            }
+        }
+    }
+
+    void pauseGame()
+    {
+        Time.timeScale = 0;
+        displayText.gameObject.SetActive(true);
+        displayText.text = "Paused";
+        displayText.fontSize = 32;
+        displayText.alignment = TextAlignmentOptions.Center;
+    }
+
+    void coutinueGame()
+    {
+        Time.timeScale = 1;
+        displayText.fontSize = 18;
+        displayText.alignment = TextAlignmentOptions.Left;
+        displayText.gameObject.SetActive(false);
     }
 }
